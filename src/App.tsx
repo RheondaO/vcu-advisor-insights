d4351735660257d5ad05f629f988ea8e37e4130c
rheonda.do <35320005+RheondaO@users.noreply.github.com>
2026-02-02 06:44:36 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import { initScrollbarHiding } from "@/libs/hideScrollbars";
+import { initScrollbarHiding } from "@/lib/hideScrollbars";
 
 const queryClient = new QueryClient();
 
