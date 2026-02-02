e503929b5fdcf8ac9107b741be2c18c0414ee99f
rheonda.do <35320005+RheondaO@users.noreply.github.com>
2026-02-02 08:38:12 -0500

    Update scroll-area.tsx

--- src/components/ui/scroll-area.tsx
+++ src/components/ui/scroll-area.tsx
@@ -1,7 +1,3 @@
-import * as React from "react";
-import * as ScrollAreaPrimitive from "@radix-ui/react-scroll-area";
-import { cn } from "@/lib/utils";
-
 const ScrollArea = React.forwardRef<
   React.ElementRef<typeof ScrollAreaPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.Root>
@@ -11,35 +7,26 @@ const ScrollArea = React.forwardRef<
     className={cn("relative overflow-hidden", className)} 
     {...props}
   >
-    {/* This Viewport handles the actual scrolling math - WE KEEP THIS */}
-    <ScrollAreaPrimitive.Viewport className="h-full w-full rounded-[inherit] [scrollbar-width:none] [-ms-overflow-style:none] [&::-webkit-scrollbar]:display-none">
+    {/* The magic is here: 
+      1. h-full w-full makes it fit.
+      2. overflow-y-scroll forces it to behave.
+      3. The custom classes for webkit/firefox hide the bar.
+    */}
+    <ScrollAreaPrimitive.Viewport 
+      className="h-full w-full rounded-[inherit] !overflow-y-scroll"
+      style={{ 
+        scrollbarWidth: 'none', 
+        msOverflowStyle: 'none',
+        WebkitOverflowScrolling: 'touch' 
+      }}
+    >
       {children}
     </ScrollAreaPrimitive.Viewport>
     
-    {/* We keep the component here for internal compatibility but it won't render anything visible */}
-    <ScrollBar />
+    {/* We strictly do not render <ScrollBar /> here */}
+    
     <ScrollAreaPrimitive.Corner />
   </ScrollAreaPrimitive.Root>
 ));
-ScrollArea.displayName = ScrollAreaPrimitive.Root.displayName;
-
-const ScrollBar = React.forwardRef<
-  React.ElementRef<typeof ScrollAreaPrimitive.ScrollAreaScrollbar>,
-  React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.ScrollAreaScrollbar>
->(({ className, orientation = "vertical", ...props }, ref) => (
-  <ScrollAreaPrimitive.ScrollAreaScrollbar
-    ref={ref}
-    orientation={orientation}
-    /* This set of classes forces the bar to be 0px wide/tall and invisible */
-    className={cn(
-      "flex touch-none select-none transition-colors !hidden !w-0 !h-0 opacity-0 pointer-events-none",
-      className
-    )}
-    {...props}
-  >
-    <ScrollAreaPrimitive.ScrollAreaThumb className="hidden !w-0 !h-0" />
-  </ScrollAreaPrimitive.ScrollAreaScrollbar>
-));
-ScrollBar.displayName = ScrollAreaPrimitive.ScrollAreaScrollbar.displayName;
 
 export { ScrollArea, ScrollBar };
